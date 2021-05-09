fc3540d74643afcc287f96fb0e4eae2152e3040c
robertlongmendez <robertlongmendez@gmail.com>
2021-05-09 14:42:35 -0500

    not sure

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
 
-@Component ({
+/*@Component ({
   selector: 'app-alert-warning',
   template: './warning-alert/warning-alert.html'
 })
